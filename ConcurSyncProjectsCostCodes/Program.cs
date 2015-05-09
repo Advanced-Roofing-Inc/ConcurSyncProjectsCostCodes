c91ca777a63f7a158c0a662aaee9234699b0b08d
Dylan Foster <dylantf@gmail.com>
2015-05-09 11:28:31 -0400

    Initial commit

---
+++ ConcurSyncProjectsCostCodes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ConcurSyncProjectsCostCodes
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var connectionString = ConfigurationManager.ConnectionStrings["SL"].ConnectionString;
+         var dataSource = new SqlConnection(connectionString);
+      }
+   }
+}
